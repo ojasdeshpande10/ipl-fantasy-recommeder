bb49e8cbf0e82a9fe0578d2ad9035112b6c7506c
ojasdeshpande10 <ojas.deshpande@learner.manipal.edu>
2020-11-06 02:34:31 +0530

    third change

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,62 @@ class App extends Component {
   render(){
   return (
     <div>
-    <div class="split left">
-    <div class="centered">
+    <div class="mydiv">
+      <h1>Fantasy11 Recommender</h1>
+    </div>
+    <div class="grid-container">
+    <div class="grid-child purple">
       
       <h2>Team1</h2>
       <p><Picks />
+
           
       </p>
-    </div>
+    
+  
   </div>
   
-  <div class="split right">
-    <div class="centered">
+  
+    <div class="grid-child green">
      
       <h2>Team2</h2>
       <p><Ticks /></p>
     </div>
   </div>
+  <div class="mydiv1">
+
+  <h3>Description List</h3>
+
+<dl>
+  <dt>Must Have Players</dt>
+  <dd>- <p>these players have performed throughout the season and almost all fantasy players will be having them in their teams. If you are entering a smaller contest these players are very important to your sucess.</p></dd>
+  <dt>Safe Bets</dt>
+  <dd>- <p>these players have performed in patches, these players will also be present in many fantasy teams. To stand out from the rest of the fantasy players you need to make one of them captain in your team.</p></dd>
+  <dt>High Risk High Reward</dt>
+  <dd>-<p>these players have not performed well but are proven match winners and can perform well at any moment. These players performing well can stand you in good stead to win the bigger contest competing with many fantasy players </p></dd>
+
+
+
+
+</dl>
+<div class="mydiv2">
+<p>This classification is based on the average of fantasy points the players have accumalated throughout the season, the idea is to create a eleven which comprises of all these categories and choosing captain is based on which kind of contest you are taking part in.</p>
+  </div>
+
+
+  
+</div>
+
+  
+
+
+
+
+
+
+
+
+
   </div>
   );
   }
